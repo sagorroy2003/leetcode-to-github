fb3088111859c97190f8999bf083834e2788cc48
Sagor Roy <sagor2003roy@gmail.com>
2026-06-23 17:53:28 +0600

    Time: 0 ms (100%), Space: 11.7 MB (83.92%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
         auto it = remove(nums.begin(),nums.end(),val);
-        nums.erase(it,nums.end());
-        return nums.size();
+        // nums.erase(it,nums.end());
+        return (it - nums.begin());
     }
 };
