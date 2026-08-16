39c1f8f72269372497b59973470f16e817208f5a
Sagor Roy <sagor2003roy@gmail.com>
2026-08-16 16:58:03 +0600

    Time: 0 ms (100%), Space: 9.3 MB (83.34%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -3,26 +3,20 @@ public:
     bool isIsomorphic(string s, string t) {
         int mapST[256];
         int mapTS[256];
-        
-        fill(mapTS, mapTS + 256 , -1);
+
+        fill(mapTS, mapTS + 256, -1);
         fill(mapST, mapST + 256, -1);
 
-
         for (int i = 0; i < s.size(); i++) {
-            if (mapST[s[i]] == -1 && mapTS[t[i]] == -1) // both not mapped
-            {
-                mapST[s[i]] = t[i];
-                mapTS[t[i]] = s[i];
-            } 
-            else if(mapST[s[i]] != -1 && mapST[s[i]] != t[i]){
+            if (mapST[s[i]] != -1 && mapST[s[i]] != t[i]) {
+                return false;
+            } else if (mapTS[t[i]] != -1 && mapTS[t[i]] != s[i]) {
                 return false;
             }
-            else if(mapTS[t[i]] != -1 && mapTS[t[i]] != s[i]){
-                return false;
-            }
+            mapST[s[i]] = t[i];
+            mapTS[t[i]] = s[i];
         }
 
-
-        return true; 
+        return true;
     }
 };
